6d7833255e6c9b5a2b7cfa374ef2a60a7f4946bc
Adham Omari <adhamomarics@gmail.com>
2023-05-20 04:19:04 +0300

    HomePagefixs

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Spinner from './Component/Spinner/spinner'
 import NotFound from './Component/Notfound'
 import NavbarApp from './Component/Navbar/NavbarApp'
 import Footer from './Component/Footer/footer'
-import HomePage from './page/Home/HomePage'
+import HomePage from '/page/Home/HomePage'
 
 const Heero = lazy(() => import('./page/Brief/Heero'))
 const StoreLocation = lazy(() => import('./page/Location/StoreLocation'))
