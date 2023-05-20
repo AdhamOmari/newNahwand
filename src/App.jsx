692ff890aca6b23b446e292f4cd326a85d4ca65a
Adham Omari <adhamomarics@gmail.com>
2023-05-20 04:10:35 +0300

    colons

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Spinner from './Component/Spinner/spinner'
 import NotFound from './Component/Notfound'
 import NavbarApp from './Component/Navbar/NavbarApp'
 import Footer from './Component/Footer/footer'
-const HomePage = lazy(() => import('./page/Home/HomePage'))
+import HomePage from './page/Home/HomePage'
 const Heero = lazy(() => import('./page/Brief/Heero'))
 const StoreLocation = lazy(() => import('./page/Location/StoreLocation'))
 const AllMenus = lazy(() => import('./page/Menu/Menus'))
