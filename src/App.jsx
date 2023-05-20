2b73f1f537bc00f4403de5eb5bf91b08d6b15a56
Adham Omari <adhamomarics@gmail.com>
2023-05-20 04:15:49 +0300

    HomePagefix

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { lazy, Suspense } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
-import { Helmet, HelmetProvider } from 'react-helmet-async'
 import Spinner from './Component/Spinner/spinner'
 import NotFound from './Component/Notfound'
 import NavbarApp from './Component/Navbar/NavbarApp'
@@ -19,26 +18,24 @@ const ChefSelection = lazy(() => import('./page/ChefSelection/ChefSelection'))
 
 const App = () => {
   return (
-    <HelmetProvider>
-      <BrowserRouter>
-        <NavbarApp />
+    <BrowserRouter>
+      <NavbarApp />
 
-        <Suspense fallback={<Spinner />}>
-          <Routes>
-            <Route path='/' element={<HomePage />} />
-            <Route path='/Herero' element={<Heero />} />
-            <Route path='/Location' element={<StoreLocation />} />
-            <Route path='/menu' element={<AllMenus />} />
-            <Route path='/menu/:category' element={<ShowMenuItem />} />
-            <Route path='/RateUs' element={<RatingButton />} />
-            <Route path='/Delivery' element={<FoodOrderType />} />
-            <Route path='/ChefSelection' element={<ChefSelection />} />
-            <Route path='*' element={<NotFound />} />
-          </Routes>
-        </Suspense>
-        <Footer />
-      </BrowserRouter>
-    </HelmetProvider>
+      <Suspense fallback={<Spinner />}>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/Herero' element={<Heero />} />
+          <Route path='/Location' element={<StoreLocation />} />
+          <Route path='/menu' element={<AllMenus />} />
+          <Route path='/menu/:category' element={<ShowMenuItem />} />
+          <Route path='/RateUs' element={<RatingButton />} />
+          <Route path='/Delivery' element={<FoodOrderType />} />
+          <Route path='/ChefSelection' element={<ChefSelection />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </Suspense>
+      <Footer />
+    </BrowserRouter>
   )
 }
 
