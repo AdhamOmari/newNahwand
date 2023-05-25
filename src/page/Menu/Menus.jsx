86693e4e63edbb2564e5c09556c51ee0343d21e9
Adham Omari <adhamomarics@gmail.com>
2023-05-25 04:40:33 +0300

    fix the responsive item menuesimage s س

--- src/page/Menu/Menus.jsx
+++ src/page/Menu/Menus.jsx
@@ -5,6 +5,7 @@ import styles from './style.module.css'
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import Spinner from '../../Component/Spinner/spinner'
+import { Helmet } from 'react-helmet'
 
 const AllMenus = () => {
   const navigate = useNavigate()
@@ -14,7 +15,7 @@ const AllMenus = () => {
 
   useEffect(() => {}, [isArabic])
   const move = (cat, item) => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+    window.scrollTo({ top: 0, behavior: 'smooth' })
 
     navigate(`/menu/${cat}`, { state: item })
   }
@@ -23,11 +24,19 @@ const AllMenus = () => {
     return <Spinner />
   }
 
+  const pageTitle = isArabic === 'arabic' ? 'قائمة الطعام ' : 'Menu - Nahwand'
+  const pageDescription =
+    isArabic === 'arabic'
+      ? 'استكشف قائمة الطعام لمطعم نهاوند'
+      : 'Explore the menu of Nahwand restaurant'
+
   return (
     <main className={styles.wrap}>
-      <h1 className={styles.menuTitle}>
-        {isArabic === 'arabic' ? 'قائمة الطعام - نهاوند' : 'Menu - Nahwand'}
-      </h1>
+      <Helmet>
+        <title>{pageTitle}</title>
+        <meta name='description' content={pageDescription} />
+      </Helmet>
+      <h1 className={styles.menuTitle}>{pageTitle}</h1>
       <div className={styles.menuContainer}>
         {Object.entries(menuData).map(([category, { image, item }]) => (
           <div key={category} className={styles.menuCategory}>
