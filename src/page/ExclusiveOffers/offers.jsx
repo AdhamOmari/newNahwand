530ce3126ba3a2b55155bc075d040ddcb82ea282
Adham Omari <adhamomarics@gmail.com>
2023-05-25 17:34:05 +0300

    fix allفثءف

--- src/page/ExclusiveOffers/offers.jsx
+++ src/page/ExclusiveOffers/offers.jsx
@@ -10,6 +10,11 @@ const Offers = () => {
     return <Spinner />
   }
 
+  const offersDescription =
+    'باكج الخير  - نصف دجاجة من البرياني المميز - محمرة , تبولة , سلطة جرجير , بابا غنوج, حمص , متبل , متبل شمندر , حمص بيروتي -  ٣حبات كبة مقلية  , ٢ من كريمة الثوم , - ٣ حبات مقبلات ساخنة - 8 اسياخ مشاوي مشكلة (لحم ودجاج) - 179 ريال شامل الضريبة'
+
+  const offerItems = offersDescription.split(' - ')
+
   const textAlignmentClass = language === 'arabic' ? styles.rtl : styles.ltr
 
   return (
@@ -24,6 +29,11 @@ const Offers = () => {
             className={styles.itemImage}
             loading='lazy' // Add this attribute for lazy-loading
           />
+          <div className={styles.offerDescription}>
+            {offerItems.map((item, index) => (
+              <div key={index}>{item}</div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
